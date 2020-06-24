b12c6fd528b35f0e488e07b957307eeb9ffbcb3f
GEORGE <mousej92@gmail.com>
2020-06-24 09:03:23 +0300

    11 coomit
    расписал все задачки

--- lesson1/js.js
+++ lesson1/js.js
@@ -41,19 +41,39 @@ console.log(expenses);
 
 // 5)
 // console.log( "1"[0] );
-// ответ:1 НЕТ +
+// ответ:1, бо НЕТ +
 
 // 6)
-// console.log(0 || 2);
+// console.log(2 && 1 && null && 0 && undefined);
+// ответ: null;
 
+// 7)
 // var a,b;
+// console.log(!!(a && b)); console.log(a && b);
+// ответ: false в первом и undefined во втором
 
-// a = [1, 2, 3]; b = [1, 2, 3];
-// if ("вжикff12" > "бблоко") {
+// 8)
+// console.log(null || 2 && 3 || 4 );
+// Ответ: 3;
+
+// 9)
+// var a,b;
+//  a = [1, 2, 3]; b = [1, 2, 3];
+// if (a == b) {
+//     console.log("klas");
+// }else{
+//     console.log("zopa");
+// }
+// ответ: жопа;
+
+// 10)
+// if ("ёжик" > "яблоко") {
 //     console.log("a");
 // }else{
 //     console.log(b);
 // }
+// ответ:ежик), прикол в порядке букв по алфавиту 
 
-
-// console.log( 0 ||"" );
+// 11)
+// console.log(0 || "" || 2 || undefined || true || falsе);
+// ответ: 2
